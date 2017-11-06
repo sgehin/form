1d804dc5981cedf6234ae2d0416d3a962bdbf86c
sgehin <sfa.gehin@gmail.com>
2017-11-06 22:07:40 +0100

    submit naar aparte php pagina

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
     <head>
         <link rel="shortcut icon" href="img/icon-form.png" />
         <title>Formulier</title>
-        
-
+        <link rel="stylesheet" type="text/css" href="myStyle.css">
     </head>
     <body>
-        <form>
-            test<input></input>
-        </form>
-        
-        
-        
+        <table>
+            <tr>
+                <td>
+                <form action="action_page.php" method="GET">
+                <br>
+                 First name: <input type="text" name="FirstName" value="Firstname"><br>
+                Last name: <input type="text" name="LastName" value="Lastname"><br>
+                <input type="submit" value="Submit">
+                </form>
+                </td>
+            </tr>
+        </table>
+                
         
     </body>
 </html>

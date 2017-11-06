8ad05c15b686fc9f61010d7742b7d80eb9572953
sgehin <sfa.gehin@gmail.com>
2017-11-06 20:03:48 +0100

    formulier met input veld

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/icon-form.png" />
+        <title>Formulier</title>
+        
+
+    </head>
+    <body>
+        <form>
+            test<input></input>
+        </form>
+        
+        
+        
+        
+    </body>
+</html>

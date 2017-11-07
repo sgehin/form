e4c3f94fe0b13f2c1881d5ff1b5a8cef013782cb
sgehin <sfa.gehin@gmail.com>
2017-11-07 19:59:17 +0100

    input veld als verplicht invullen gedefinieerd met html

--- action_page.php
+++ action_page.php
@@ -6,42 +6,37 @@
         <link rel="shortcut icon" href="img/icon-form.png" />
         <title>Formulier</title>
         <link rel="stylesheet" type="text/css" href="myStyle.css">
-        <script>
-        function back(){
-            onclick:href="index.php";
-        }
-        </script>
     </head>
     <body>
         <?php
-        $gender=$_GET['Gender'];
-        $fname=$_GET['FirstName']; 
-        $lname=$_GET['LastName'];
-        $DateOfBirth=$_GET['DateOfBirth'];
-        $country=$_GET['country'];
-        $Street=$_GET['Street'];
-        $Zipcode=$_GET['Zipcode'];
-        $City=$_GET['City'];
-        $usremail=$_GET['usremail'];
+            $gender=$_GET['Gender'];
+            $fname=$_GET['FirstName']; 
+            $lname=$_GET['LastName'];
+            $DateOfBirth=$_GET['DateOfBirth'];
+            $country=$_GET['country'];
+            $Street=$_GET['Street'];
+            $Zipcode=$_GET['Zipcode'];
+            $City=$_GET['City'];
+            $usremail=$_GET['usremail'];
                 
         $handle = fopen('fileA.txt','a');
-        fwrite($handle,'GENDER : '.$gender."\n");
-        fwrite($handle,'FIRSTNAME : '.$fname."\n");
-        fwrite($handle,'LASTNAME : '.$lname."\n");
-        fwrite($handle,'DATE OF BIRTH : '.$DateOfBirth."\n");
-        fwrite($handle,'COUNTRY : '.$country."\n");
-        fwrite($handle,'STREETNAME : '.$Street."\n");
-        fwrite($handle,'ZIPCODE : '.$Zipcode."\n");
-        fwrite($handle,'CITY : '.$City."\n");
-        fwrite($handle,'EMAIL : '.$usremail."\n");
-        fwrite($handle,'-----------------------------------------'."\n");
+            fwrite($handle,'GENDER        : '.$gender."\n");
+            fwrite($handle,'FIRSTNAME     : '.$fname."\n");
+            fwrite($handle,'LASTNAME      : '.$lname."\n");
+            fwrite($handle,'DATE OF BIRTH : '.$DateOfBirth."\n");
+            fwrite($handle,'COUNTRY       : '.$country."\n");
+            fwrite($handle,'STREETNAME    : '.$Street."\n");
+            fwrite($handle,'ZIPCODE       : '.$Zipcode."\n");
+            fwrite($handle,'CITY          : '.$City."\n");
+            fwrite($handle,'EMAIL         : '.$usremail."\n");
+            fwrite($handle,'-----------------------------------------'."\n");
         fclose($handle);
         
-        echo "Thank you for filling in the form.";
-                   
+        echo "Thank you for filling out the form.";
+                
         ?>
         <br><br>
-        <button><a href="index.php">back</a></button>
+        <button><a href="index.php">Back</a></button>
         
         
     </body>
--- index.php
+++ index.php
@@ -6,21 +6,20 @@
         <link rel="stylesheet" type="text/css" href="myStyle.css">
     </head>
     <body>
-        <p><img src='New-Circle.png'><h2>Registration of Personalia</h2>
+        <p> <h2>Registration of Personalia</h2>
         <h6>For externals</h6>
-        </p> <!-- img werktniet -->
-        
-        <table>
+             
+        <table> 
             <tr>
-                <td>
+                <td background="img/white.jpg" width=300px"> 
                     <p><h4>Personal information</h4></p>
-                <form action="action_page.php" method="GET">
-                    <input type="radio" name="Gender" value="Female">Female<br>
-                    <input type="radio" name="Gender" value="Male">Male<br><br> 
-                    First name: <input type="text" name="FirstName" value=""><br>
-                    Last name: <input type="text" name="LastName" value=""><br><br>
-                    Date of birth<input type="text" name="DateOfBirth" value="" maxlength="10" size="10">dd-mm-yyyy<br><br>
-                        Country:<input list="country" name="country">
+                    <form action="action_page.php" method="GET">
+                        <input type="radio" name="Gender" value="Female">Female<br>
+                        <input type="radio" name="Gender" value="Male">Male<br><br> 
+                        First name: <input type="text" name="FirstName" value="" required><br>
+                        Last name: <input type="text" name="LastName" value="" required><br><br>
+                        Date of birth<input type="text" name="DateOfBirth" value="" maxlength="10" size="10">dd-mm-yyyy<br><br>
+                        Country:<input list="country" name="country" required>
                                 <datalist id="country">
                                     <option value="Belgie">
                                     <option value="Duitsland">
@@ -28,16 +27,19 @@
                                     <option value="Frankrijk">
                                     <option value="Nederland">
                                 </datalist>
-                    <p><h4>Private address</h4></p>
-                    <input type="text" name="Street" value="">Street<br>
-                    <input type="text" name="Zipcode" value="">Zipcode<br>
-                    <input type="text" name="City" value="">City<br><br>
-                    E-mail: <input type="email" name="usremail">                                      
-                    <input type="submit" value="Submit">       <!-- submit knop verdwenen van formulier????-->
-                </form>
+                        <p><h4>Private address</h4></p>
+                        <input type="text" name="Street" value="" required>Street<br>
+                        <input type="text" name="Zipcode" value="">Zipcode<br>
+                        <input type="text" name="City" value="" required>City<br><br>
+                    
+                        E-mail: <input type="email" name="usremail" required>                                      
+                        <input type="submit" value="Submit"><br>       
+                    </form>
                 </td>
             </tr>
-        </table>
+        
+        
+       
                 
         
     </body>
